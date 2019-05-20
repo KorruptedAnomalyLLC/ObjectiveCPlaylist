d1fffa6b684fae1bd518a986e4dcd33c6246c1a0
Austin West <korruptedanomaly@gmail.com>
2019-05-20 11:49:24 -0600

    Playlist Controllers finished except 1 error

--- OBJCPlaylist/OBJCPlaylist/ModelControllers/AJWPlaylistController.m
+++ OBJCPlaylist/OBJCPlaylist/ModelControllers/AJWPlaylistController.m
@@ -8,6 +8,54 @@
 
 #import "AJWPlaylistController.h"
 
+@interface AJWPlaylistController()
+
+@property (nonatomic, strong, readwrite) NSMutableArray* internalPlaylists;
+
+@end
+
 @implementation AJWPlaylistController
+
++ (AJWPlaylistController*) sharedController
+{
+    static AJWPlaylistController *sharedController = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedController = [AJWPlaylistController new];
+    });
+    return sharedController;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _internalPlaylists = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void)createPlaylistWithName:(NSString *)name
+{
+    AJWPlaylist *newPlaylist = [[AJWPlaylist alloc] initWithName:name
+                                                           songs: [NSMutableArray new]];
+    [self.internalPlaylists addObject:newPlaylist];
+}
+
+- (void)deletePlaylist:(AJWPlaylist *)playlist
+{
+    [self.internalPlaylists removeObject:playlist];
+}
+
+- (void)addSongWithTitle:(NSString *)title andArtist:(NSString *)artist toPlaylist:(AJWPlaylist *)playlist
+{
+    AJWSong *newSong = [[AJWSong alloc] initWithTitle:title artist:artist];
+    [playlist addSongsObject:newSong];
+}
+
+- (void)removeSong:(AJWSong *)song fromPlaylist:(AJWPlaylist *)playlist
+{
+    [playlist removeSongsObject:song];
+}
 
 @end
